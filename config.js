43d01aa92babedf53f3a12c142132f7cb61bfca9
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-23 13:47:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ global.limitawal = {
     premium: "Infinity",
     free: 25
 }
-global.thumb = fs.readFileSync('./lib/naze.png')
+global.thumb = fs.readFileSync('./lib/menu.png')
 global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
 
 let file = require.resolve(__filename)
